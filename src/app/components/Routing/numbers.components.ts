06977a221e75c6021defbe56a73118da2e1e40f8
greg <greg@loud-20.04>
2021-10-19 18:37:18 -0400

    added service.ts for user-info and now pulls data into the file before then using it elsewhere.

--- src/app/components/Routing/numbers.components.ts
+++ src/app/components/Routing/numbers.components.ts
@@ -3,36 +3,66 @@
 import { Component, Injectable, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { UserInfo } from "../user-info.model";
+import { UserInfoService } from "../user-info.service";
 
 
+// @Injectable()
+// @Component({    
+//     selector:"numbers-component",
+//     templateUrl:"numbers.components.html"
+    
+//     })    
 
-@Injectable()
-@Component({    
+// export class NumbersComponent implements OnInit{
+//     myInfo: UserInfo | undefined;
+//     constructor(private http: HttpClient){
+//     }
+//     ngOnInit(): void {
+//         console.log("Sending A Get Request");
+//         this.getUserInfo();
+//         console.log("Got User Info")
+//         this.showUserInfo();
+//     }
+
+//     getUserInfo(){
+//         return this.http.get<UserInfo>('https://skype-app-23623-default-rtdb.firebaseio.com/my-info.json')
+
+//     }
+
+//     showUserInfo(){
+//         this.getUserInfo().subscribe ((data: UserInfo)=>{
+//             console.log(data);
+//             this.myInfo = data;
+//         })
+//     }
+// }
+@Component({
     selector:"numbers-component",
     templateUrl:"numbers.components.html"
     
-    })    
-
-export class NumbersComponent implements OnInit{
-    myInfo: UserInfo | undefined;
-    constructor(private http: HttpClient){
+    })   
+    
+    export class NumbersComponent implements OnInit{
+        myInfo: UserInfo | undefined;
+    
+        constructor(private userInfoService:UserInfoService){
+        }
+    
+    
+    
+        ngOnInit(): void {
+            console.log("Sending A Get Request");
+            this.userInfoService.getUserInfo();
+            console.log("Got User Info")
+            this.showUserInfo();
+        }
+    
+    
+    
+        showUserInfo(){
+            this.userInfoService.getUserInfo().subscribe ((data: UserInfo)=>{
+                console.log(data);
+                this.myInfo = data;
+            })
+        }
     }
-    ngOnInit(): void {
-        console.log("Sending A Get Request");
-        this.getUserInfo();
-        console.log("Got User Info")
-        this.showUserInfo();
-    }
-
-    getUserInfo(){
-        return this.http.get<UserInfo>('https://skype-app-23623-default-rtdb.firebaseio.com/my-info.json')
-
-    }
-
-    showUserInfo(){
-        this.getUserInfo().subscribe ((data: UserInfo)=>{
-            console.log(data);
-            this.myInfo = data;
-        })
-    }
-}
--- src/app/components/user-info.component.ts
+++ src/app/components/user-info.component.ts
@@ -1,9 +1,10 @@
 import { Component, Injectable, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+
 import { UserInfo } from "./user-info.model";
+import { UserInfoService } from "./user-info.service";
 
 
-@Injectable()
+
 @Component({
 selector: "skype-user-info",
 templateUrl: "user-info.component.html"
@@ -11,22 +12,23 @@ templateUrl: "user-info.component.html"
 
 export class UserInfoComponent implements OnInit{
     myInfo: UserInfo | undefined;
-    constructor(private http: HttpClient){
+
+    constructor(private userInfoService:UserInfoService){
     }
+
+
+
     ngOnInit(): void {
         console.log("Sending A Get Request");
-        this.getUserInfo();
+        this.userInfoService.getUserInfo();
         console.log("Got User Info")
         this.showUserInfo();
     }
 
-    getUserInfo(){
-        return this.http.get<UserInfo>('https://skype-app-23623-default-rtdb.firebaseio.com/my-info.json')
 
-    }
 
     showUserInfo(){
-        this.getUserInfo().subscribe ((data: UserInfo)=>{
+        this.userInfoService.getUserInfo().subscribe ((data: UserInfo)=>{
             console.log(data);
             this.myInfo = data;
         })
